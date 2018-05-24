14796fe8bbc19dd81954bbdf40f1f6a8b1711129
Low Jeng Lam <jenglamlow@gmail.com>
2018-05-24 14:52:24 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Example1 from './Example1';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Example1/>
+      </div>
+    );
+  }
+}
+
+export default App;
